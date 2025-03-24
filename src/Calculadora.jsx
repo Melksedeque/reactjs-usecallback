d7ca0d71d59b97d2c417369539e08db5dbbdd6d5
Melksedeque <freelancer@melksedeque.com.br>
2025-03-24 20:44:27 -0300

    Criação de forms para receber os numeros; Definição da hipotenusa para chamar o cálculo

--- src/Calculadora.jsx
+++ src/Calculadora.jsx
@@ -1,15 +1,41 @@
-function Calculadora() {
+import { useEffect } from "react";
+import { useState } from "react";
+
+function Calculadora({ formulaPitagoras }) {
+  const [ladoB, setLadoB] = useState(0);
+  const [ladoC, setLadoC] = useState(0);
+  const [, setHipotenusa] = useState(0);
+
+  useEffect(() => {
+    if (ladoB > 0 && ladoC > 0) {
+      setHipotenusa(formulaPitagoras(ladoB, ladoC));
+    }
+  }, [ladoB, ladoC, formulaPitagoras]);
+
   return (
     <div>
-      <h1>Calculadora</h1>
+      <h1>Calculadora de Pitágoras</h1>
       <div>
         <label htmlFor="">Lado B</label>
-        <input type="text" />
+        <input
+          type="number"
+          placeholder="Insira o valor de B"
+          value={ladoB}
+          onChange={(e) => setLadoB(e.target.value)}
+        />
       </div>
       <div>
         <label htmlFor="">Lado C</label>
-        <input type="text" />
+        <input
+          type="number"
+          placeholder="Insira o valor de C"
+          value={ladoC}
+          onChange={(e) => setLadoC(e.target.value)}
+        />
       </div>
+      <p>
+        O valor da hipotenusa é: <strong>???</strong>
+      </p>
     </div>
   );
 }
