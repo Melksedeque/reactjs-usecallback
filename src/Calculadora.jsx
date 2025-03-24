4511ea48698d52d45393d75bdec0705cb221b922
Melksedeque <freelancer@melksedeque.com.br>
2025-03-24 20:34:31 -0300

    Criação do componente inicial de Calculadora

---
+++ src/Calculadora.jsx
@@ -0,0 +1,17 @@
+function Calculadora() {
+  return (
+    <div>
+      <h1>Calculadora</h1>
+      <div>
+        <label htmlFor="">Lado B</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label htmlFor="">Lado C</label>
+        <input type="text" />
+      </div>
+    </div>
+  );
+}
+
+export default Calculadora;
