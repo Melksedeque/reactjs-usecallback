094a7838c3aa552e0c6a9ca7dc28fbfa65d3eb00
Melksedeque <freelancer@melksedeque.com.br>
2025-03-24 20:47:39 -0300

    Criação da fórmula de Pitagoras

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import "./App.css";
 import Calculadora from "./Calculadora";
 
 function App() {
+  const formulaPitagoras = (ladoB, ladoC) => {
+    const quadradoB = Math.pow(ladoB, 2);
+    const quadradoC = Math.pow(ladoC, 2);
+    const somaQuadrados = quadradoB + quadradoC;
+    return Math.sqrt(somaQuadrados);
+  };
+
   return (
     <div className="pitagoras">
       <img
@@ -9,7 +16,7 @@ function App() {
         alt="Triângulo retângulo de Pitagoras"
         width={500}
       />
-      <Calculadora />
+      <Calculadora formulaPitagoras={formulaPitagoras} />
     </div>
   );
 }
