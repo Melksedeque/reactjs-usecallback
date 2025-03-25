6189c067fbc40cbe8bc264f3f5d1908f5909c56e
Melksedeque <freelancer@melksedeque.com.br>
2025-03-24 21:22:01 -0300

    Estilização com Tailwind

--- src/Calculadora.jsx
+++ src/Calculadora.jsx
@@ -13,27 +13,31 @@ function Calculadora({ formulaPitagoras }) {
   }, [ladoB, ladoC, formulaPitagoras]);
 
   return (
-    <div>
-      <h1>Calculadora de Pitágoras</h1>
-      <div>
-        <label htmlFor="">Lado B: </label>
+    <div className="flex flex-col align-items-center justify-center text-center">
+      <h1 className="text-5xl font-bold text-white mt-5 mb-7">
+        Calculadora de Pitágoras
+      </h1>
+      <div className="flex flex-col align-items-center justify-center mb-5">
+        <label className="text-xl text-white">Valor do Lado B:</label>
         <input
-          type="number"
+          className="border-1 border-gray-300 rounded-3xl text-white text-center p-2 placeholder:text-gray-200 placeholder:italic"
+          type="text"
           placeholder="Insira o valor de B"
           value={ladoB}
           onChange={(e) => setLadoB(e.target.value)}
         />
       </div>
-      <div>
-        <label htmlFor="">Lado C: </label>
+      <div className="flex flex-col align-items-center justify-center mb-5">
+        <label className="text-xl text-white">Valor do Lado C:</label>
         <input
-          type="number"
+          className="border-1 border-gray-300 rounded-3xl text-white text-center p-2 placeholder:text-gray-200 placeholder:italic"
+          type="text"
           placeholder="Insira o valor de C"
           value={ladoC}
           onChange={(e) => setLadoC(e.target.value)}
         />
       </div>
-      <p>
+      <p className="text-white text-3xl mt-5">
         O valor da hipotenusa é: <strong>{hipotenusa}</strong>
       </p>
     </div>
