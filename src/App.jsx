f26c9ab1214a114d97747b52b27aff2a360d06a0
Melksedeque <freelancer@melksedeque.com.br>
2025-03-24 21:25:41 -0300

    Uso do callback para referenciar a formula de Pitagoras

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+import { useCallback } from "react";
 import Calculadora from "./Calculadora";
 
 function App() {
-  const formulaPitagoras = (ladoB, ladoC) => {
+  const formulaPitagoras = useCallback((ladoB, ladoC) => {
     const quadradoB = Math.pow(ladoB, 2);
     const quadradoC = Math.pow(ladoC, 2);
     const somaQuadrados = quadradoB + quadradoC;
     return Math.sqrt(somaQuadrados);
-  };
+  }, []);
 
   return (
     <div className="app">
